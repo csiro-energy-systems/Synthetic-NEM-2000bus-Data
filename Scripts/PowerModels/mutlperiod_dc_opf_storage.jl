2b59c075df410d82a7955ebc03524acdbd4ea420
Frederik <frederik.geth@gmail.com>
2025-02-12 11:09:21 +1000

    Update mutlperiod_dc_opf_storage.jl

--- Scripts/PowerModels/mutlperiod_dc_opf_storage.jl
+++ Scripts/PowerModels/mutlperiod_dc_opf_storage.jl
@@ -1,17 +1,16 @@
 using PowerModels
 using JuMP
-using Ipopt
 using Plots
 using DataFrames
 using HiGHS
 const _PM = PowerModels
 
-optimizer = JuMP.optimizer_with_attributes(HiGHS.Optimizer)
+optimizer_highs = JuMP.optimizer_with_attributes(HiGHS.Optimizer)
 
 ## 
 data_file = "snem1803_storage.m"
 data = PowerModels.parse_file(data_file)
-result_dcp_opf = _PM._solve_opf_strg(data, DCPPowerModel, optimizer, setting = Dict("output" => Dict("duals" => true)))
+result_dcp_opf = _PM._solve_opf_strg_mi(data, DCPPowerModel, optimizer_highs, setting = Dict("output" => Dict("duals" => true)))
 obj_snem1803 = result_dcp_opf["objective"]
 time_snem1803 = result_dcp_opf["solve_time"]
 
@@ -24,10 +23,12 @@ data["time_series"] = Dict(
     )
 )
 mn_data = make_multinetwork(data)
-result_dc_storage_opf = _PM.solve_mn_opf_strg(mn_data, DCPPowerModel, optimizer,  setting = Dict("output" => Dict("duals" => true)))
+result_dc_storage_opf = _PM.solve_mn_opf_strg(mn_data, DCPPowerModel, optimizer_highs,  setting = Dict("output" => Dict("duals" => true)))
 obj_snem1803_storage = result_dc_storage_opf["objective"]
 time_snem1803_storage = result_dc_storage_opf["solve_time"]
 
+# duals of kcl at buses are stored in solution -> nw is time step index -> bus index -> lam_kcl_r, 
+duals_ts_1 = Dict(b=> bus["lam_kcl_r"] for (b,bus)  in result_dc_storage_opf["solution"]["nw"]["1"]["bus"])
+# but they will be zeros in presence of binary variables. see https://jump.dev/JuMP.jl/stable/tutorials/linear/mip_duality/
 ##
-pg_dc = Dict(gen["pg"] => id for (id, gen) in result_dcp_opf["solution"]["gen"])
-qg_dc = Dict(gen["qg"] => id for (id, gen) in result_dcp_opf["solution"]["gen"])
+pg_dc = Dict(gen["pg"] => id for (id, gen) in result_dc_storage_opf["solution"]["nw"]["1"]["gen"])
